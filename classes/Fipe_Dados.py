b15012e836d69671ab85d1c693a1e04875e50958
RobertCardosoo <robertrcp70@gmail.com>
2023-03-10 03:08:02 -0300

    Adição de base de dados de marcas de moto

--- classes/Fipe_Dados.py
+++ classes/Fipe_Dados.py
@@ -1,5 +1,5 @@
 import json
-with open("classes/marcas.json",encoding='utf-8') as meu_json:
+with open("classes/marcas_carros.json",encoding='utf-8') as meu_json:
     marcas = json.load(meu_json)
     
 class Fipe():
